9d950cc1c246a97247ab77aa63e47895b09e7426
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2025-02-13 13:23:53 +0530

    Update lab04_q3.py

--- lab04_q3.py
+++ lab04_q3.py
@@ -1,14 +1,12 @@
 def pan_gram(a):
-    b=set(a.lower())
-    if ' ' in b:
-        if len(b)==27:
-            return 'pangram'
-        else:
-            return 'not pangram'
+    b=set()
+    for i in a:
+        if(i.isalpha()):
+            b.add(i)
+    if len(b)==26:
+        return "pangram"
     else:
-        if len(b)==26:
-            return 'pangram'
-        else:
-            return 'not pangram'
+        return "not pangram"
+    
 Sent=input()
 print(pan_gram(Sent))
