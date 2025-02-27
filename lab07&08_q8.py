c53414e824eae3dfb984b9c381618669b4a037e0
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2025-02-27 16:47:03 +0530

    Update lab07&08_q8.py

--- lab07&08_q8.py
+++ lab07&08_q8.py
@@ -1,46 +1,27 @@
-def decode_message(encoded_message):
-    def decode(index, current_decoded, decoded_messages):
-        if index == len(encoded_message):
-            decoded_messages.append("".join(current_decoded))
-            return
+"""
+Decode the message:
+A message containing the letters from A-Z can be encoded into the numbers using the mapping
+A-> 1, B-> 2, C-> 3, ..., Z-> 26. To decode an encoded message, you need to group the digits
+and do the reverse mapping. You are required to display all the possible decoded messages.
+For example: "11106" can be decoded into:
+a. "AAJF" with the grouping (1 1 10 6)
+b. "KJF" with the grouping (11 10 6)
+"""
 
-        # Decode one digit
-        one_digit = int(encoded_message[index])
-        if 1 <= one_digit <= 9:
-            current_decoded.append(chr(one_digit + 64)) 
-            decode(index + 1, current_decoded, decoded_messages)
-            current_decoded.pop()  # Backtrack
+def decode_message(message, result=""):
+    if not message:
+        print(result)
+        return
 
-        # Decode two digits (if possible)
-        if index + 1 < len(encoded_message):
-            two_digits = int(encoded_message[index:index + 2])
-            if 10 <= two_digits <= 26:
-                current_decoded.append(chr(two_digits + 64)) 
-                decode(index + 2, current_decoded, decoded_messages)
-                current_decoded.pop()  # Backtrack
+    if message[0] != "0":
+        decode_message(message[1:], result + chr(int(message[:1]) + 64))
 
-    decoded_messages = []
-    decode(0, [], decoded_messages)
-    return decoded_messages
+    if len(message) > 1 and "10" <= message[:2] <= "26":
+        decode_message(message[2:], result + chr(int(message[:2]) + 64))
 
+def decode_menu():
+    msg = input("Enter encoded message: ")
+    print("Possible decodings:")
+    decode_message(msg)
 
-encoded_message = "11106"
-decoded_messages = decode_message(encoded_message)
-print(f"Encoded message: {encoded_message}")
-print("Possible decoded messages:")
-for message in decoded_messages:
-    print(message)
-
-encoded_message2 = "123"
-decoded_messages2 = decode_message(encoded_message2)
-print(f"\nEncoded message: {encoded_message2}")
-print("Possible decoded messages:")
-for message in decoded_messages2:
-    print(message)
-
-encoded_message3 = "226"
-decoded_messages3 = decode_message(encoded_message3)
-print(f"\nEncoded message: {encoded_message3}")
-print("Possible decoded messages:")
-for message in decoded_messages3:
-    print(message)
+decode_menu()
